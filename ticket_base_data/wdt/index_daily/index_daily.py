de62897c00ed06c86aa0426776d011c485b7bd7a
RedRain01 <1596964137@qq.com>
2025-05-23 16:13:32 +0800

    db  setting

--- ticket_base_data/wdt/index_daily/index_daily.py
+++ ticket_base_data/wdt/index_daily/index_daily.py
@@ -5,38 +5,25 @@ import mysql.connector
 from io import IOBase
 from mysql.connector import Error
 from datetime import datetime, timedelta
+from ticket_base_data.doris_connect.dorise_db import doris_db
 from ticket_base_data.base_db.job_base import  base_job_update,base_create_job
 
 
-def save_to_dorisdb(df, host, port, user, password, database):
-    try:        # 连接到 DorisDB 数据库
-        with mysql.connector.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            database=database
-        ) as conn:
-            # 创建一个游标对象
-            with conn.cursor() as cursor:
-                # 根据选中的列动态生成插入数据的 SQL 语句
-                columns = df.columns.tolist()
-                placeholders = ', '.join(['%s'] * len(columns))
-                insert_query = f"""
-                INSERT INTO ticket_index_daily (
-                    {', '.join(columns)}
-                ) VALUES (
-                    {placeholders}
-                )
-                """
-                # 将 DataFrame 中的数据插入到表中，使用批量插入提高性能
-                data_to_insert = [tuple(row) for row in df.values]
-                cursor.executemany(insert_query, data_to_insert)
-                # 提交事务
-                conn.commit()
-
+def save_to_dorisdb(df):
+    try:
+        columns = df.columns.tolist()
+        placeholders = ', '.join(['%s'] * len(columns))
+        insert_query = f"""
+                        INSERT INTO demo.wdt_index_daily (
+                            {', '.join(columns)}
+                        ) VALUES (
+                            {placeholders}
+                        )
+                        """
+        # 将 DataFrame 中的数据插入到表中，使用批量插入提高性能
+        data_to_insert = [tuple(row) for row in df.values]
+        doris_db.execute_many(insert_query, data_to_insert)
         print("数据已成功保存到 DorisDB 数据库")
-
     except Error as e:
         print(f"数据库操作出现错误: {e}")
 
@@ -62,7 +49,7 @@ def main():
     code = "index_daily"
     jobflag = True
     params = ("指数日线行情", "index_daily", date.today(), date.today(), datetime.now(), 1, "1", "2",)
-    result=base_create_job(code, "2025-03-14",  "2025-03-17", params)
+    result=base_create_job(code, "2020-01-01",  "2025-05-13", params)
     token = result.data['token']
     id = result.data['id']
     start_date = result.data['start_date']
@@ -70,52 +57,30 @@ def main():
     if result.error:
         return
     try:
-            # 连接到 DorisDB 数据库
-            with mysql.connector.connect(
-                    host='192.168.0.106',  # 替换为你的 DorisDB 主机地址
-                    port=9030,  # 替换为你的 DorisDB 端口号
-                    user='root',  # 替换为你的用户名
-                    password='why123',  # 替换为你的密码
-                    database='demo',  # 替换为你的数据库名称
-            ) as conn:
-                # 创建一个游标对象
-                with conn.cursor() as cursor:
-                    # 执行查询获取 ticket 表的所有数据
-                    cursor.execute("SELECT * FROM ticket_base_list")
-                    # cursor.execute("SELECT ts_code FROM ticket_base_list WHERE ts_code NOT IN ( SELECT ts_code  FROM ticket_index_daily )")
-                    # 获取所有查询结果
-                    rows = cursor.fetchall()
-                    ts.set_token(token)
-                    pro = ts.pro_api()
-                    #
-                    num = 0
-                    for row in rows:
-                        num += 1
-                        if num ==20:
-                            break
-                        print(f"all--num:{len(rows)}---run--num:{num}")
-                        #   df = pro.index_daily(ts_code='000036.SZ', start_date='20180101', end_date='20181010')
-                        start_date =convert_to_string(start_date)
-                        end_date =convert_to_string(end_date)
-                        df = pro.daily(ts_code=row[0], start_date=start_date, end_date=end_date)
-                        time.sleep(0.03)
-                        if df is not None and not df.empty:
-                            if jobflag:
-                                jobflag = False
-                                base_job_update(id, 0)
-                            save_to_dorisdb(df,
-                                            host='192.168.0.106',  # 替换为你的 DorisDB 主机地址
-                                            port=9030,  # 替换为你的 DorisDB 端口号
-                                            user='root',  # 替换为你的用户名
-                                            password='why123',  # 替换为你的密码
-                                            database='demo'  # 替换为你的数据库名称
-                                            )
-                        else:
-                            continue
-                    if not jobflag:
-                        base_job_update(id, 9)
-                    print(f"任务执行完成:{jobflag}")
-                    print(f"任务更新结果：{result}")
+        rows = doris_db.execute_query("SELECT * FROM ticket_base_list", None, False)
+        ts.set_token(token)
+        pro = ts.pro_api()
+        #
+        num = 0
+        for row in rows:
+            num += 1
+            print(f"all--num:{len(rows)}---run--num:{num}")
+            #   df = pro.index_daily(ts_code='000036.SZ', start_date='20180101', end_date='20181010')
+            start_date = convert_to_string(start_date)
+            end_date = convert_to_string(end_date)
+            df = pro.daily(ts_code=row['ts_code'], start_date=start_date, end_date=end_date)
+            time.sleep(3)
+            if df is not None and not df.empty:
+                if jobflag:
+                    jobflag = False
+                    base_job_update(id, 0)
+                save_to_dorisdb(df )
+            else:
+                continue
+        if not jobflag:
+            base_job_update(id, 9)
+        print(f"任务执行完成:{jobflag}")
+        print(f"任务更新结果：{result}")
     except Error as e:
      print(f"数据库操作出现错误: {e}")
 def convert_to_string(value):
